fd9722224b552a4437a5423c21aa6af3a5a0956e
Elan Ruusamäe <glen@pld-linux.org>
2022-03-22 19:58:57 +0200

    Feature: Add results variable definition to index.ts

--- src/app/templates/ts/src/index.ts
+++ src/app/templates/ts/src/index.ts
@@ -2,8 +2,13 @@
 // and are not exported out.
 //
 import {DangerDSLType} from "../node_modules/danger/distribution/dsl/DangerDSL"
+import {DangerResults} from "../node_modules/danger/distribution/dsl/DangerResults"
 
 declare var danger: DangerDSLType
+/**
+ * Current results of Danger run instance
+ */
+declare var results: DangerResults
 declare function message(message: string): void
 declare function warn(message: string): void
 declare function fail(message: string): void
